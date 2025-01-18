0cb85838160570f22dc5fa22c2b8ec78141a6614
Lukas-K1 <lukas.koepsel@freenet.de>
2025-01-18 16:52:39 +0100

    Update demo.py
    moved to somewaht functioning state

--- src/demo.py
+++ src/demo.py
@@ -1,26 +1,28 @@
-import gymnasium
-from bppy import *
+import gym
 import highway_env
+from bppy import *
 from matplotlib import pyplot as plt
 
-# Define the system
-env = gymnasium.make('highway-v0', render_mode='rgb_array', config={"lanes_count": 4, "vehicles_count": 2, "duration": 40, "initial_spacing": 2, "road": "highway"})
+
+env = gymnasium.make('highway-v0', render_mode='rgb_array',
+                     config={"lanes_count": 4, "vehicles_count": 2, "duration": 40, "initial_spacing": 2,
+                             "road": "highway"})
 obs = env.reset()
 
-# Define the specification
+
 CHANGE_LEFT = Bool("left")
 CHANGE_RIGHT = Bool("right")
 
 
 @thread
 def change_lane_left():
-    for i in range(10):
+    for i in range(100):
         yield sync(request=CHANGE_LEFT)
 
 
 @thread
 def change_lane_right():
-    for i in range(10):
+    for i in range(100):
         yield sync(request=CHANGE_RIGHT)
 
 
@@ -34,45 +36,27 @@ def regulator():
             e = yield sync(block=CHANGE_RIGHT, waitFor=true)
 
 
-def map_action_to_highwayenv(event):
+def map_action_to_highwayenv(event: Bool):
     if event == CHANGE_LEFT:
-        return 2  # Spurwechsel links
+        return 0  # Spurwechsel links
     elif event == CHANGE_RIGHT:
-        return 3  # Spurwechsel rechts
-    return 0
+        return 2  # Spurwechsel rechts
+    return 1 # idle
 
 
-def collision_detected(environment):
-    for vehicle in environment.metadata["vehicle_count"]:
-        if vehicle.crashed:
-            return True
-    return False
+if __name__ == '__main__':	
+    program = BProgram(bthreads=[change_lane_left(), change_lane_right(), regulator()],
+                    event_selection_strategy=SMTEventSelectionStrategy(), listener=PrintBProgramRunnerListener())
 
 
-def init_bpprogram():
-    return BProgram(bthreads=[change_lane_left(), change_lane_right(), regulator()],
-                          event_selection_strategy=SMTEventSelectionStrategy(), listener=BProgramRunnerListener())
-
-
-if __name__ == '__main__':
-    event_list = [CHANGE_LEFT, CHANGE_RIGHT]
-    logger = EventLogger()
-
-    program = init_bpprogram()
     done = False
-    counter = 0
     while not done:
-        program.run()
-        ev = program.
-        action = map_action_to_highwayenv(ev)
-        obs = env.step(action)
-        if counter == 8:
-            done = True
-        done = done# or collision_detected(env)
-        counter += 1
+        bp_program.run()
+        event = bp_program.next_event()
+        action = map_action_to_highwayenv(event)
+        obs, reward, done, info = env.step(action)
         env.render()
         plt.imshow(env.render())
         plt.show()
 
-# plt.imshow(env.render())
-# plt.show()
+env.close()
