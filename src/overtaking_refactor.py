309e229b16e8772ff9dcd54fe8cf17724aba6859
Lukas-K1 <lukas.koepsel@freenet.de>
2025-02-16 22:19:19 +0100

    updating logic to fulfil overtake requirements
     both vehicles stopping wip

--- src/overtaking_refactor.py
+++ src/overtaking_refactor.py
@@ -17,98 +17,93 @@ def create_env(config: Dict[str, Any]) -> gym.Env:
     return env
 
 
+def process_observation(obs: Any) -> List[List[float]]:
+    return obs #if isinstance(obs, list) #else obs.tolist()
+
+
 def decide_action(vehicles: List[List[float]], lane_width: float = 4.0) -> int:
     ego_vehicle = vehicles[0]
-    ego_position = ego_vehicle[1]
-    ego_lane = int(round(ego_vehicle[2] / lane_width))
-    ego_speed = ego_vehicle[3]
-
-    # print("ego: ", ego_lane, ego_position, ego_speed)
+    ego_position, ego_lane, ego_speed = ego_vehicle[1], int(round(ego_vehicle[2] / lane_width)), ego_vehicle[3]
 
     if len(vehicles) == 1:
         return 1
 
     other_vehicle = vehicles[1]
-    other_position = other_vehicle[1]
-    other_lane = int(round(other_vehicle[2] / lane_width))
-    other_speed = other_vehicle[3]
-
-    # print("other: ", other_lane, other_position, other_speed)
+    other_position, other_lane, other_speed = other_vehicle[1], int(round(other_vehicle[2] / lane_width)), other_vehicle[3]
 
     same_lane = other_lane == ego_lane
     other_is_in_front = other_position > ego_position
     distance_between_vehicles = abs(other_position - ego_position)
-    ego_is_faster = other_speed - ego_speed < 0
+    ego_is_faster = ego_speed > other_speed
 
     if other_is_in_front:
         if same_lane and distance_between_vehicles < 25:
-            if ego_lane > 0:
-                return 0  # Nach links wechseln
-            else:
-                return 2  # Nach rechts wechseln
-        elif other_speed - ego_speed < -5:
-            return 4  # bremsen
-        else:
-            return 3  # beschleunigen
-    elif same_lane:
-        return 4  # bremsen
-    elif distance_between_vehicles > 5 and ego_is_faster:
-        if other_lane - ego_lane < 0:
-            return 0  # Nach links wechseln
-        else:
-            return 2  # Nach rechts wechseln
-
-    return 1  # Idle
+            return 0 if ego_lane > 0 else 2
+        elif not same_lane:
+            return 1
+        return 4 if other_speed - ego_speed < -5 else 3
+    elif same_lane and distance_between_vehicles > 50 and ego_is_faster and not other_is_in_front:
+        return 4
+    elif not other_is_in_front and distance_between_vehicles > 15 and ego_is_faster:
+        if ego_lane - other_lane < 0:
+            return 2
+        elif ego_lane - other_lane > 0:
+            return 0
+        return 3
+    elif same_lane and distance_between_vehicles > 10:
+        return 3  # Speed up to approach leading car
+    elif same_lane and distance_between_vehicles > 45 or not same_lane:
+        return 1  # Idle
+    return 4 if same_lane else (
+        0 if other_lane - ego_lane < 0 else 2) if distance_between_vehicles > 5 and ego_is_faster else 1
 
 
 def decide_passiv(vehicles: List[List[float]], lane_width: float = 4.0) -> int:
-    ego_vehicle = vehicles[[0]]
-    ego_position = ego_vehicle[1]
-    ego_lane = int(round(ego_vehicle[2] / lane_width))
-    ego_speed = ego_vehicle[3]
+    ego_vehicle = vehicles[0]
+    ego_position, ego_lane, ego_speed = ego_vehicle[1], int(round(ego_vehicle[2] / lane_width)), ego_vehicle[3]
 
     if len(vehicles) == 1:
         return 1
 
     other_vehicle = vehicles[1]
-    other_position = other_vehicle[1]
-    other_lane = int(round(other_vehicle[2] / lane_width))
-    other_speed = other_vehicle[3]
+    other_position, other_lane, other_speed = other_vehicle[1], int(round(other_vehicle[2] / lane_width)), other_vehicle[3]
 
     same_lane = other_lane == ego_lane
     other_is_in_front = other_position > ego_position
     distance_between_vehicles = abs(other_position - ego_position)
-    ego_is_faster = other_speed - ego_speed < 1
+    ego_is_faster = ego_speed > other_speed
 
     if same_lane and other_is_in_front and distance_between_vehicles < 25 and ego_is_faster:
         return 4
-
-    return 1
+    elif not other_is_in_front and distance_between_vehicles > 35 and other_position > 0:
+        return 4  # Speed up to maintain position
+    elif not other_is_in_front and distance_between_vehicles > 35 and not other_position > 0:
+        return 1  # Speed up to maintain position
+    elif not other_is_in_front and 35 > distance_between_vehicles > 10:
+        return 1  # Speed up to maintain position
+    elif other_is_in_front and 35 > distance_between_vehicles > 10:
+        return 1  # Speed up to maintain position
+    elif other_is_in_front and distance_between_vehicles > 10 and ego_position < other_position:
+        return 3  # Speed up to maintain position
+    return 4
 
 
 @thread
 def check_clearance(obs, env):
     while True:
         yield sync(request=CHECK_CLEARANCE)
-        #obs, reward, done, terminate, info = env.step((1, 1))
-        vehicles = obs
-        #actions = decide_overtake_action(vehicles)
-        action_0 = decide_action(vehicles)
-        action_1 = decide_passiv(vehicles)
-        print("Actions: ", action_0, action_1)
+        vehicles = process_observation(obs)
+        action_0, action_1 = decide_action(vehicles[0]), decide_passiv(vehicles[1])
 
-        if action_0 in [0, 2] or action_1 in [0, 2]:
+        if action_0 in [0, 2] and action_1 not in [0, 2, 3] or action_0 == 3 and action_1 in [1, 4]:
             yield sync(request=START_OVERTAKE)
-            obs = env.step((action_0, action_1))
-            env.render()
-        elif action_0 in [3, 4] or action_1 in [3, 4]:
+        elif action_0 in [3, 4] and action_1 in [0,1,2,3] or action_0 == 1 and action_1 in [0, 2, 3,4]:
             yield sync(request=CHECK_CLEARANCE)
-            obs = env.step((action_0, action_1))
-            env.render()
         else:
             yield sync(request=ABORT_OVERTAKE)
-            obs = env.step((action_0, action_1))
-            env.render()
+
+        obs, _, _, _, _ = env.step((action_0, action_1))
+        env.render()
 
 
 @thread
@@ -161,21 +156,20 @@ def main():
                 "type": "DiscreteMetaAction",
                 "longitudinal": True,
                 "lateral": True,
-                "target_speeds": [0, 5, 10, 15, 20, 25, 30],
-                # Die dikreten Geschwindigkeiten, die das Auto annehmen kann
+                "target_speeds": [0, 5, 10, 15, 20, 25, 30]
             },
         },
+        "simulation_frequency": 100,
+        "initial_positions": [[15, 1, 20], [45, 1, 2]]  # Fixed start positions
     }
 
     env = create_env(config)
-    obs, info = env.reset(seed=1)
-    bp = BProgram(bthreads=[check_clearance(obs, env), perform_overtake(), abort_overtake(), complete_overtake()], event_selection_strategy=SimpleEventSelectionStrategy(), listener=PrintBProgramRunnerListener())
+    obs, _ = env.reset()
+    bp = BProgram(bthreads=[check_clearance(obs, env), perform_overtake(), abort_overtake(), complete_overtake()],
+                  event_selection_strategy=SimpleEventSelectionStrategy(), listener=PrintBProgramRunnerListener())
     bp.run()
 
     for _ in range(100):
-        # obs, info = env.reset(seed=1)
-        # bp = BProgram(bthreads=[check_clearance(obs, env), perform_overtake(), abort_overtake(), complete_overtake()],
-        #               event_selection_strategy=SimpleEventSelectionStrategy(), listener=PrintBProgramRunnerListener())
         bp.run()
         env.render()
         env.step((1, 1))
