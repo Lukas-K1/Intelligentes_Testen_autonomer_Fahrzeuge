d70f36eed981ffd8b07a033c39da3ffbba152646
Lukas-K1 <lukas.koepsel@freenet.de>
2025-02-22 22:10:33 +0100

    merge main in feature

--- src/main.py
+++ src/main.py
@@ -5,11 +5,57 @@ import highway_env as highway
 from bppy import *
 
 # BPpy Events
+change_right = BEvent("change_right")
+change_left = BEvent("change_left")
+increase_speed = BEvent("increase_speed")
+reduce_speed = BEvent("reduce_speed")
+keep_speed = BEvent("keep_speed")
+
 
 def create_env(config: Dict[str, Any]) -> gym.Env:
     env = gym.make('highway-v0', render_mode='rgb_array', config=config)
     env.reset()
     return env
+
+
+@thread
+def change_lane_right():
+    yield sync(waitFor=change_right)
+
+
+@thread
+def change_lane_left():
+    yield sync(waitFor=change_left)
+
+
+@thread
+def speed_increase():
+    yield sync(waitFor=increase_speed)
+
+
+@thread
+def speed_reduce():
+    yield sync(waitFor=reduce_speed)
+
+
+@thread
+def speed_idle():
+    yield sync(waitFor=keep_speed)
+
+
+@thread
+def control_vehicle(env):
+    while True: # TODO placeholder
+        yield sync(request=change_right)
+        yield sync(request=change_left)
+        yield sync(request=increase_speed)
+        yield sync(request=reduce_speed)
+        yield sync(request=keep_speed)
+
+
+@thread
+def control_events():
+    print("Control events") # TODO placeholder
 
 
 def set_config():
@@ -55,15 +101,16 @@ def set_config():
 
 
 def main():
+
     config = set_config()
     env = create_env(config)
     obs, _ = env.reset()
-    # bp = BProgram(bthreads=[],
-    #               event_selection_strategy=SimpleEventSelectionStrategy(), listener=PrintBProgramRunnerListener())
-    # bp.run()
+    bp = BProgram(bthreads=[change_lane_right(), change_lane_left(), speed_increase(), speed_reduce(), speed_idle(), control_vehicle(env)],
+                  event_selection_strategy=SimpleEventSelectionStrategy(), listener=PrintBProgramRunnerListener())
+    #bp.run()
 
     for _ in range(100):
-        #bp.run()
+        bp.run()
         obs = env.step((1, 1, 2, 4, 0, 4, 4))
         print(obs)
         env.render()
