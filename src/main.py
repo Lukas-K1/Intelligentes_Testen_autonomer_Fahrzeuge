815aad7de654db35df322b1ff26db163cfce7997
Lukas-K1 <lukas.koepsel@freenet.de>
2025-02-25 18:12:57 +0100

    update to use same_lane check
    and merged main in branch

--- src/main.py
+++ src/main.py
@@ -7,11 +7,11 @@ from bppy import *
 from observation_wrapper import ObservationWrapper
 
 # BPpy Events
-change_right = BEvent("change_right")
-change_left = BEvent("change_left")
-increase_speed = BEvent("increase_speed")
-reduce_speed = BEvent("reduce_speed")
-keep_speed = BEvent("keep_speed")
+change_lane = BEvent("LANE_CHANGE")
+update_position = BEvent("POSITION_UPDATE")
+increase_speed = BEvent("SPEED_UP")
+new_step = BEvent("STEP")
+update_speed = BEvent("SPEED_UPDATE")
 
 
 def create_env(config: Dict[str, Any]) -> gym.Env:
@@ -20,64 +20,79 @@ def create_env(config: Dict[str, Any]) -> gym.Env:
     return env
 
 
-def decide_action(vehicles: List[List[float]], obs_wrapper: ObservationWrapper) -> int:
-    if obs_wrapper.get_distance_to_leading_vehicle(0) > 35:
+def decide_action(obs_wrapper: ObservationWrapper, v_id: int, space: float) -> int:
+    """
+    Decides the action to be taken by the vehicle based on the current observation.
+    :param obs_wrapper: The observation wrapper containing functionality based on the current observation.
+    :param v_id: The id of the vehicle.
+    :param space: The space to be considered for the action.
+    :return: The action to be taken.
+    """
+    distance = obs_wrapper.get_distance_to_leading_vehicle(v_id)
+    right_clear = obs_wrapper.is_right_lane_clear(v_id, space, space)
+    left_clear = obs_wrapper.is_left_lane_clear(v_id, space, space)
+    same_lane = obs_wrapper.is_in_same_lane(v_id,0)
+
+    if distance > 35 and same_lane:
         return 3
-    if (obs_wrapper.is_right_lane_clear(0, 5, 5) and obs_wrapper.get_distance_to_leading_vehicle(0) < 10 and not obs_wrapper.is_left_lane_clear(0, 5, 5)):
+    if distance < 10 and right_clear and not left_clear and same_lane:
         return 2
-    if (not obs_wrapper.is_right_lane_clear(0, 5, 5) and obs_wrapper.get_distance_to_leading_vehicle(0) < 10 and not obs_wrapper.is_left_lane_clear(0, 5, 5)):
+    if not right_clear and distance < 10 and not left_clear and same_lane:
         return 4
-    if (not obs_wrapper.is_right_lane_clear(0, 5, 5) and obs_wrapper.get_distance_to_leading_vehicle(0) < 10 and obs_wrapper.is_left_lane_clear(0, 5, 5)):
+    if not right_clear and distance < 10 and left_clear and same_lane:
         return 0
-    if 35 > obs_wrapper.get_distance_to_leading_vehicle(0) > 5:
+    if 35 > distance > 5:
         return 1
     return 1
     # TODO rework if information about lane placement is available
 
 
-@thread
-def change_lane_right():
-    yield sync(waitFor=change_right)
+def position_update(distance: float) -> BEvent:
+    return BEvent("POSITION_UPDATE", data={"distance_to_vut": distance})
 
 
-@thread
-def change_lane_left():
-    yield sync(waitFor=change_left)
+def speed_update(speed: float) -> BEvent:
+    return BEvent("SPEED_UPDATE", data={"speed": speed})
 
 
-@thread
-def speed_increase():
-    yield sync(waitFor=increase_speed)
+def speed_increase(step: int) -> BEvent:
+    return BEvent("SPEED_UP", data={"step": step})
 
 
-@thread
-def speed_reduce():
-    yield sync(waitFor=reduce_speed)
+def make_step() -> BEvent:
+    return BEvent("STEP")
 
 
-@thread
-def speed_idle():
-    yield sync(waitFor=keep_speed)
+def change_the_lane(step: int) -> BEvent:
+    return BEvent("LANE_CHANGE", data={"step": step})
 
+
+def make_end() -> BEvent:
+    return BEvent("END")
 
 @thread
 def control_vehicle(env, obs, obs_wrapper):
+    step = 0
     while True:  # TODO placeholder
+        step += 1
         obs_wrapper.set_observation(obs)
-        action = decide_action(obs, obs_wrapper)
+        action = decide_action(obs_wrapper, 1, 10)
         print(action)
         if action == 0:
-            yield sync(request=change_left)
+            yield sync(request=change_the_lane(step))
         if action == 1:
-            yield sync(request=keep_speed)
+            print("Action 1")
         if action == 2:
-            yield sync(request=change_right)
+            yield sync(request=change_the_lane(step))
         if action == 3:
-            yield sync(request=increase_speed)
+            yield sync(request=speed_increase(step))
         if action == 4:
-            yield sync(request=reduce_speed)
+            print("Action 4")
         obs,  _, _, _, _ = env.step((action, 1))
         env.render()
+        yield sync(request=position_update(obs_wrapper.get_distance_to_leading_vehicle(1)))
+        yield sync(request=speed_update(obs_wrapper.get_velocity(1)))
+        yield sync(request=make_step())
 
 
 @thread
@@ -90,17 +105,16 @@ def set_config():
         "centering_position": [0.5, 0.5],
         "vehicles_count": 1,
         #"controlled_vehicles": 7,
-        "controlled_vehicles": 2,
+        "controlled_vehicles": 6,
         "lanes_count": 4,
         "initial_positions": [
-            [15, 2, 32],  # (x_position, lane_index, speed)
-            [45, 2, 20],
-            [65, 3, 27],
-            #[105, 0, 15],
-            #[135, 3, 35],
-            #[165, 1, 17],
-            #[195, 3, 23],
-            #[225, 0, 29]
+            [65, 3, 20],  # (x_position, lane_index, speed)
+            [15, 3, 20],
+            [85, 0, 20],
+            [105, 0, 20],
+            [55, 0, 20],
+            [45, 1, 25],
+            [25, 3, 25]
         ],  # Fixed start positions WIP
         "observation": {
             "type": "MultiAgentObservation",
@@ -133,15 +147,12 @@ def main():
     env = create_env(config)
     obs, _ = env.reset()
     obsw = ObservationWrapper(obs)
-    bp = BProgram(bthreads=[change_lane_right(), change_lane_left(), speed_increase(), speed_reduce(), speed_idle(),
-                            control_vehicle(env, obs, obsw)],
+    bp = BProgram(bthreads=[control_vehicle(env, obs, obsw)],
                   event_selection_strategy=SimpleEventSelectionStrategy(), listener=PrintBProgramRunnerListener())
-    #bp.run()
+    bp.run()
 
     for _ in range(100):
-        bp.run()
-        #obs = env.step((1, 1, 2, 4, 0, 4, 4))
-        obs = env.step((1, 1))
+        obs = env.step((1, 1, 1, 1, 1, 1))
         print(obs)
         env.render()
 
